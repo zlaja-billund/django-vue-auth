c36be3480d010baa7861284cc23e7801efe56acf
zlaja-billund <mail@zlatan.dk>
2024-11-07 17:26:51 +0100

    register screendump added

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ import os
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 from rest_framework import generics, status
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from .serializers import RegisterSerializer, ResetPasswordSerializer
 from .models import User, PasswordReset
@@ -15,32 +16,34 @@ class RegisterView(generics.ListCreateAPIView):
 
 
 class ResetPasswordView(generics.GenericAPIView):
+    serializer_class = ResetPasswordSerializer
 
     def post(self, request):
         serializer = ResetPasswordSerializer(data=request.data)
 
+        # If request requst email is missing return validation error
         try:
             email = request.data['email']
         except:
-            return Response({"error": "Bad request"}, status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError({"error": "Bad request input"})
 
         user = User.objects.filter(email=email).first()
 
-        if user:
-            token_generator = PasswordResetTokenGenerator()
-            token = token_generator.make_token(user)
+        if not user:
+            raise ValidationError({'error': 'User with credentials not found'})
 
-            # Insert new password reset object in db
-            PasswordReset.objects.update_or_create(
-                user=user,
-                defaults={'token': token}
-            )
+        token_generator = PasswordResetTokenGenerator()
+        token = token_generator.make_token(user)
 
-            reset_url = f"{os.environ['PASSWORD_RESET_BASE_URL']}/{token}"
+        # Insert new password reset object in db
+        PasswordReset.objects.update_or_create(
+            user=user,
+            defaults={'token': token}
+        )
 
-            # TODO: implement here to send an email
+        reset_url = f"{os.environ['PASSWORD_RESET_BASE_URL']}/{token}"
 
-            return Response({'success': 'We have sent you a link to reset your password'}, status=status.HTTP_200_OK)
-        else:
-            return Response({"error": "User with credentials not found"}, status=status.HTTP_404_NOT_FOUND)
+        # TODO: implement here to send an email
+
+        return Response({'success': 'We have sent you a link to reset your password'}, status=status.HTTP_200_OK)
 
